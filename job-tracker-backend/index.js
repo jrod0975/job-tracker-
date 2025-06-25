0a84ab8b42c44d3eceb1449a810c5a9c72cdc340
Josue Rodriguez <jrod09752015@gmail.com>
2025-06-24 22:52:40 -0500

    First commit

---
+++ job-tracker-backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({message: "This is the backend"})
+})
+
+app.post("/", (req, res) => {
+    const {username, password} = req.body
+    console.log("Recieved login attempt: ", username, password)
+
+    if (username === "test" && password === "test"){
+        res.json({success: true, message: "Login Success"})
+    }
+    else{
+        res.json({success: false, message: "Login Failed"})
+    }
+})
+
+app.listen(PORT, () =>{
+    console.log("Server runnning on port " + PORT)
+})
